20b8c935f45e2c4f55b5e9f15af9353a04227cfa
姚旭 <xu.yao@zeropartner.com>
2023-01-13 13:46:37 +0800

    update init prompt

--- libs/commands/init.js
+++ libs/commands/init.js
@@ -1,22 +1,80 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
+import ora from "ora";
+import { getTemplateList } from "./list.js";
 
-async function init() {
+// 展示模版列表
+function showTemplate(list) {
+  list.forEach((item) => {
+    console.log(
+      "  " +
+        chalk.red("★") +
+        "  " +
+        chalk.green(item.name) +
+        " - " +
+        item.description
+    );
+  });
+  console.log();
+}
+
+// 命令行交互
+async function prompt(templateList, templateNames) {
   const answers = await inquirer.prompt([
     {
       type: "input",
       name: "projectName",
       message: "请输入项目名称：",
+      validate(val) {
+        if (val === "") {
+          console.log(chalk.yellow("项目名称不能为空."));
+          return;
+        } else {
+          return true;
+        }
+      },
     },
     {
-      type: "list",
+      type: "input",
       name: "templateName",
       message: "请选择模版：",
-      default: "vue",
-      choices: ["vue", "vue-ts", "vue-basic"],
+      validate(val) {
+        if (val === "") {
+          console.log(chalk.yellow("模板名称不能为空"));
+          return;
+        }
+        if (templateNames.indexOf(val) == -1) {
+          console.log();
+          console.log(chalk.red("无此模板，请从下列模版中创建："));
+          console.log();
+          showTemplate(templateList);
+          return;
+        }
+
+        return true;
+      },
     },
   ]);
 
-  console.log(answers, "answers");
+  return answers;
+}
+
+async function init() {
+  var spinner = ora("initialization template...");
+  spinner.start();
+  let res = await getTemplateList();
+  spinner.stop();
+
+  let templateList = [],
+    templateNames = [];
+
+  try {
+    templateList = JSON.parse(res);
+    templateNames = templateList.map((item) => item.name);
+  } catch (error) {}
+
+  const answers = prompt(templateList, templateNames);
+  console.log("answers", answers);
 }
 
 export default init;
--- libs/commands/list.js
+++ libs/commands/list.js
@@ -1,22 +1,41 @@
 import request from "request";
 import chalk from "chalk";
+import ora from "ora";
+
 import { TEMPLATE_URL } from "../../config/index.js";
 import logger from "../../utils/logger.js";
 
-export default function list() {
-  request(
-    {
-      url: TEMPLATE_URL,
-      headers: {
-        "User-Agent": "cowry-cli",
+export function getTemplateList() {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: TEMPLATE_URL,
+        headers: {
+          "User-Agent": "cowry-cli",
+        },
       },
-    },
-    function (err, res, body) {
-      if (err) logger.fail(err);
+      function (err, res, body) {
+        if (err) {
+          reject(err);
+          return;
+        }
 
-      console.log("  Available official templates:");
+        resolve(body);
+      }
+    );
+  });
+}
+
+export default function list() {
+  var spinner = ora("downloading template...");
+  spinner.start();
+  getTemplateList().then(
+    function (resp) {
+      spinner.stop();
+      console.log("  模版列表:");
       console.log();
-      JSON.parse(body).forEach(function (repo) {
+
+      JSON.parse(resp).forEach(function (repo) {
         console.log(
           "  " +
             chalk.red("★") +
@@ -26,6 +45,10 @@ export default function list() {
             repo.description
         );
       });
+    },
+    function (err) {
+      spinner.stop();
+      logger.fail(err);
     }
   );
 }
